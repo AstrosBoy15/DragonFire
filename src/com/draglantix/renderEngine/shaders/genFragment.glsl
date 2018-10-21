10be202031e0554914af2f64a78c6fcaada9f421
gandalphTheTransparentBlue <alex.e.pietras@gmail.com>
2018-10-20 18:33:01 -0600

    color grading

--- src/com/draglantix/renderEngine/shaders/genFragment.glsl
+++ src/com/draglantix/renderEngine/shaders/genFragment.glsl
@@ -24,19 +24,21 @@ void main(void) {
 		out_Color = vec4(1, 1, 1, 1);
 	}
 
+	float scaleColor = out_Color.x * 255;
+
 	if (colorType == true) {
 		out_Color = vec4(color * out_Color);
 	} else {
-		if (out_Color.x == 0) {
-			out_Color = vec4(c0.xyz, out_Color.w);
-		} else if (out_Color.x > 0 && out_Color.x <= 0.25) {
-			out_Color = vec4(c1.xyz, out_Color.w);
-		} else if (out_Color.x > 0.25 && out_Color.x <= 0.5) {
-			out_Color = vec4(c2.xyz, out_Color.w);
-		} else if (out_Color.x > 0.5 && out_Color.x <= 0.99) {
-			out_Color = vec4(c3.xyz, out_Color.w);
+		if (scaleColor < 51) {
+			out_Color = vec4(c0 * out_Color);
+		} else if (scaleColor >= 51 && scaleColor < 102) {
+			out_Color = vec4(c1 * out_Color);
+		} else if (scaleColor >= 102 && scaleColor < 153) {
+			out_Color = vec4(c2 * out_Color);
+		} else if (scaleColor >= 153 && scaleColor < 204) {
+			out_Color = vec4(c3 * out_Color);
 		} else {
-			out_Color = vec4(c4.xyz, out_Color.w);
+			out_Color = vec4(c4 * out_Color);
 		}
 	}
 }
