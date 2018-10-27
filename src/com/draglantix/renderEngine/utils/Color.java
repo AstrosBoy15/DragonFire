205e47bd5f2dca7dfcc831c13fc89946cc6db34e
pie-tras <alex.e.pietras@gmail.com>
2018-10-27 17:42:08 -0600

    Color Fix

--- src/com/draglantix/renderEngine/utils/Color.java
+++ src/com/draglantix/renderEngine/utils/Color.java
@@ -5,90 +5,33 @@ import org.joml.Vector4f;
 public class Color {
 
 	private float r, g, b, a;
-	private boolean converted = false;
-
+	
 	public Color(float r, float g, float b, float a) {
 		this.r = r;
 		this.b = b;
 		this.g = g;
 		this.a = a;
-		convert();
 	}
 	
 	public Color(Vector4f color) {
-		color.x = r;
-		color.y = b;
-		color.z = g;
-		color.w = a;
-		convert();
+		r = color.x;
+		b = color.y;
+		g = color.z;
+		a = color.w;
 	}
 
 	public Color getColor() {
 		return this;
 	}
-	
-	public Vector4f getColorVector() {
-		return new Vector4f(r, g, b, a);
-	}
 
-	public float getAlpha() {
-		if(converted) {
-			return a * 255;
-		} else {
-			return a;
-		}
-	}
-
-	public float getR() {
-		if(converted) {
-			return r * 255;
-		} else {
-			return r;
-		}
-	}
-
-	public float getG() {
-		if(converted) {
-			return g * 255;
-		} else {
-			return g;
-		}
-	}
-
-	public float getB() {
-		if(converted) {
-			return b * 255;
-		} else {
-			return b;
-		}
-	}
-
-	public void setAlpha(float alpha) {
-		a = alpha;
-	}
-
-	public void setR(float red) {
-		r = red;
-		convert();
-	}
-
-	public void setG(float green) {
-		g = green;
-		convert();
-	}
-
-	public void setB(float blue) {
-		b = blue;
-		convert();
-	}
-
-	public void convert() {
-		if(r > 1 || g > 1 || b > 1) {
-			r /= 255;
-			g /= 255;
-			b /= 255;
-			converted = true;
-		}
-	}
+	public float getAlpha() { return a; }
+	public float getR() { return r; }
+	public float getG() { return g; }
+	public float getB() { return b; }
+	public Vector4f getColorVector() { return new Vector4f(r, g, b, a); }
+	public void setAlpha(float alpha) { a = alpha; }
+	public void setR(float red) { r = red; }
+	public void setG(float green) { g = green; }
+	public void setB(float blue) { b = blue; }
 
 }
